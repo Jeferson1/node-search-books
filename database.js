bcd93f4208302adf6c559743e1b2f2d28b0516fb
jef <jeferson.s.silva1@gmail.com>
2021-06-29 17:19:09 -0300

    finish project

---
+++ database.js
@@ -0,0 +1,31 @@
+const livros = [
+    { 
+        id: 1,
+        name: "Black Matter",
+        autor: "William Crouch",
+        categoria: "Fiction",
+        paginas: "300",
+        recomenda: true,
+        leu: true
+    },
+    { 
+        id: 2,
+        name: "On The Road",
+        autor: "Jack Keurac",
+        categoria: "Romance",
+        paginas: "250",
+        recomenda: true,
+        leu: true
+    },
+    { 
+        id: 3,
+        name: "Call Of Chtulhu",
+        autor: "H.P. Lovecraft",
+        categoria: "Horror",
+        paginas: "200",
+        recomenda: true,
+        leu: true
+    },
+];
+
+module.exports = livros;
